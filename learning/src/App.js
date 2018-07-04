ef5745f718ed277bf4532ce1d608903f141439d5
Reneicko Getten <rgetten@vertisjm.com>
2018-07-04 07:58:30 -0500

    added graphql server

--- learning/src/App.js
+++ learning/src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       <div>
       <Header />
       <div>
-        <Post />
+        <Post alias="htk_codes" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Moving the community!" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg"  />
+        <Post alias="htk_code" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Moving the community!" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg"  />
       </div>
     </div>
     );
--- learning/src/components/posts/index.js
+++ learning/src/components/posts/index.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react'
 import './post.css'
 class Post extends Component {
+    constructor(props)
+    {
+        super(props);
+    }
   render() {
+
+    const alias=this.props.alias;
+    const avatar=this.props.avatar;
+    const image=this.props.image;
+    const caption=this.props.caption;
     return (
       
         <article className="post" ref="post">
             <header>
                 <div className="user-post">
-                    <div className="user-avatar"><img src="https://www.laravelnigeria.com/img/chris.jpg" alt="" srcset=""/>
-                    
+                    <div className="user-avatar"><img src={avatar} alt={alias} srcset=""/>
                     </div>
+                    <span>{alias}</span>
                 </div>
-                <div className="user-alias">
-                <span>htk_codes</span>
-                </div>
+              
             </header>
             <div className="post-image">
                 <div className="post-image-bg">
-                    <img src="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" alt=""/>
+                    <img src={image} alt={caption}/>
                 </div>
-            <div className="post-caption"><strong>Its Lit</strong></div>
+            <div className="post-caption"><strong>{caption}</strong></div>
             </div>
         </article>
       
